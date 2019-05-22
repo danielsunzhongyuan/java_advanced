4e42b00c61e5e33976f5b8f49b52c9f45c213580
sunzhongyuan <sunzhongyuan@lvwan.com>
2019-05-23 00:53:30 +0800

    :art: insert sort

---
+++ nowcoder/src/main/java/com/zsun/java/nowcoder/jianzhioffer/PathInTree.java
@@ -0,0 +1,30 @@
+package com.zsun.java.nowcoder.jianzhioffer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by zsun.
+ * DateTime: 2019/05/22 21:48
+ */
+public class PathInTree {
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        ArrayList<ArrayList<Integer>> results = new ArrayList<>();
+        ArrayList<Integer> currentPath = new ArrayList<>();
+        helper(results, currentPath, root, target);
+        return results;
+    }
+
+    private void helper(ArrayList<ArrayList<Integer>> results, ArrayList<Integer> currentPath, TreeNode node, int currentTarget) {
+        if (node == null) {
+            return;
+        }
+        currentPath.add(node.getValue());
+        // 必须是从根结点到叶子节点，所以要加上后面俩判断条件
+        if (currentTarget == node.getValue() && node.left == null && node.right == null) {
+            results.add((ArrayList<Integer>)currentPath.clone());
+        }
+        helper(results, (ArrayList<Integer>)currentPath.clone(), node.left, currentTarget - node.getValue());
+        helper(results, (ArrayList<Integer>)currentPath.clone(), node.right, currentTarget - node.getValue());
+    }
+}
