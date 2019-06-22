c68e8bb8d4cf4cf31c79b28aca6ab836b8c08a85
danielsunzhongyuan <daniel.sunzhongyuan@gmail.com>
2019-06-22 22:01:54 +0800

    基数排序

---
+++ algorithms/src/main/java/com/zsun/java/algorithms/sort/RadixSort.java
@@ -0,0 +1,67 @@
+package com.zsun.java.algorithms.sort;
+
+/**
+ * Created by qzou.
+ * DateTime: 2019-06-22 20:25
+ *
+ * @author qzou
+ */
+public class RadixSort implements Sort {
+    private static final int DIGIT_NUMBER = 3;
+    private static final int MAX = 10;
+    private int[] radix = new int[DIGIT_NUMBER + 1];
+
+    public RadixSort() {
+        radix[0] = 1;
+        for (int i = 1; i <= DIGIT_NUMBER; i++) {
+            radix[i] = (int) Math.pow(10.0, (double) (i - 1));
+        }
+    }
+
+    @Override
+    public void sort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return;
+        }
+        int length = array.length;
+
+        for (int d = 1; d <= DIGIT_NUMBER; d++) {
+            countingSort(array, length, d);
+        }
+    }
+
+    private void countingSort(int[] array, int n, int d) {
+        int[] counting = new int[MAX];
+        for (int i = 0; i < MAX; i++) {
+            counting[i] = 0;
+        }
+
+        for (int i = 0; i < n; i++) {
+            counting[getDigit(array[i], d)]++;
+        }
+
+        for (int i = 1; i < MAX; i++) {
+            counting[i] += counting[i - 1];
+        }
+
+        int[] tmp = new int[n];
+        int digit;
+        for (int i = n - 1; i >= 0; i--) {
+            digit = getDigit(array[i], d);
+            tmp[--counting[digit]] = array[i];
+        }
+
+        System.arraycopy(tmp, 0, array, 0, n);
+    }
+
+    /**
+     * 获取元素x的第d位数字
+     *
+     * @param x
+     * @param d
+     * @return
+     */
+    private int getDigit(int x, int d) {
+        return (x / radix[d]) % 10;
+    }
+}
