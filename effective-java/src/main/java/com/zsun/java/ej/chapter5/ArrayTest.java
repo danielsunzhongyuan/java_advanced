b2374d656139476430607024802725d76c3a0c59
danielsunzhongyuan <daniel.sunzhongyuan@gmail.com>
2019-12-17 23:48:11 +0800

    xx

---
+++ effective-java/src/main/java/com/zsun/java/ej/chapter5/ArrayTest.java
@@ -0,0 +1,16 @@
+package com.zsun.java.ej.chapter5;
+
+/**
+ * Created by qzou at 2019-11-27 21:53
+ *
+ * @author qzou
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        a[0] = 3;
+        a[1] = 2;
+        a[2] = 4;
+        System.out.println(a);
+    }
+}
